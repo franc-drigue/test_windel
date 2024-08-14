2a48f2f2a9c2fa0f982f8fabd7f06d6ba5b23b7d
Francisco <francdrigue@gmail.com>
2024-08-13 23:44:42 -0300

    start project

---
+++ src/app/(screens)/edit/page.tsx
@@ -0,0 +1,8 @@
+export default function EditPage(){
+
+    return(
+        <div>
+            <h1>Page Edit</h1>
+        </div>
+    )
+}
---
+++ src/app/(screens)/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage(){
+
+    return(
+        <div>
+            <h1>Page Register</h1>
+        </div>
+    )
+}
